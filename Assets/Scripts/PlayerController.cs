3e1f3574cf2d6f891e3f6bb5a0db9d696f4b9cf7
Nyx <nyxssmith@gmail.com>
2024-08-17 13:36:53 -0400

    player can move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rigid;
+    public float moveSpeed = 5;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    //void Start()
+    //{
+    //    rigid = GetComponent<Rigidbody>();
+    //}
     // Update is called once per frame
     void Update()
     {
-        
+        // take in motion
+        float vertical = Input.GetAxis("Vertical")*moveSpeed;
+        float horizontal = Input.GetAxis("Horizontal")*moveSpeed;
+        // apply forces to self
+        rigid.AddForce(new Vector3(0,0,vertical),ForceMode.Force);
+        rigid.AddForce(new Vector3(horizontal,0,0),ForceMode.Force);
     }
-
-    
 }
